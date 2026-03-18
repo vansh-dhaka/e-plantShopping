16d8f69a0501896d77551b366e9e2ca4b05dfb6d
vansh-dhaka <27vanshdhaka@gmail.com>
2026-03-18 06:19:42 -0400

    removing minor error

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -4,26 +4,24 @@ import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
-  const dispatch = useDispatch();
-
-  // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
-    let total = 0;
-    cart.forEach((item) => {
-        total += parseFloat(item.cost.subtring(1))*item.quantity
-    })
-    return total
-  };
+    const cart = useSelector(state => state.cart.items);
+    const dispatch = useDispatch();
+  
+    // Calculate total amount for all products in the cart
+    const calculateTotalAmount = () => {
+        let total = 0;
+        cart.forEach((item) => {
+            total += parseFloat(item.cost.substring(1))*item.quantity
+        })
+        return total
+      };
 
   const handleContinueShopping = (e) => {
    onContinueShopping(e)
   };
 
-
-
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({name: item.name, quntity: item.quantity + 1}))
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
